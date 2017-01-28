bad2cf730ac212c96a2cfae7b2b01a7e9386b0d3
Carlos <rockarlos@me.com>
2017-01-28 14:18:41 -0600

    Initial Commit

---
+++ Hack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hack
+//
+//  Created by Carlos Castellanos on 28/01/17.
+//  Copyright © 2017 Carlos Castellanos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
